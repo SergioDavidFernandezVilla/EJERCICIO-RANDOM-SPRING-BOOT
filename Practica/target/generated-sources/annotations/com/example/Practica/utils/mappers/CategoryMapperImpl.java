6c941886a088cd8ce62a809d918e45d2c9611cb5
gatocombotasfernandez <gatocombotasfernandez@gmail.com>
2025-01-01 18:55:44 +0000

    "changes files"

---
+++ Practica/target/generated-sources/annotations/com/example/Practica/utils/mappers/CategoryMapperImpl.java
@@ -0,0 +1,65 @@
+package com.example.Practica.utils.mappers;
+
+import com.example.Practica.persistence.entity.CategoryEntity;
+import com.example.Practica.presentation.controller.dto.CategoryDTO;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-01-01T18:53:24+0000",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.41.0.v20241217-1506, environment: Java 17.0.13 (Eclipse Adoptium)"
+)
+@Component
+public class CategoryMapperImpl implements CategoryMapper {
+
+    @Override
+    public CategoryDTO fromEntity(CategoryEntity categoryEntity) {
+        if ( categoryEntity == null ) {
+            return null;
+        }
+
+        String categoria = null;
+        String descripcion = null;
+        String created_at = null;
+        String updated_at = null;
+
+        categoria = categoryEntity.getCategoria();
+        descripcion = categoryEntity.getDescripcion();
+        if ( categoryEntity.getCreated_at() != null ) {
+            created_at = DateTimeFormatter.ISO_LOCAL_DATE_TIME.format( categoryEntity.getCreated_at() );
+        }
+        if ( categoryEntity.getUpdated_at() != null ) {
+            updated_at = DateTimeFormatter.ISO_LOCAL_DATE_TIME.format( categoryEntity.getUpdated_at() );
+        }
+
+        Long id = null;
+
+        CategoryDTO categoryDTO = new CategoryDTO( id, categoria, descripcion, created_at, updated_at );
+
+        return categoryDTO;
+    }
+
+    @Override
+    public CategoryEntity fromDTO(CategoryDTO categoryDTO) {
+        if ( categoryDTO == null ) {
+            return null;
+        }
+
+        CategoryEntity.CategoryEntityBuilder categoryEntity = CategoryEntity.builder();
+
+        categoryEntity.categoria( categoryDTO.categoria() );
+        if ( categoryDTO.created_at() != null ) {
+            categoryEntity.created_at( LocalDateTime.parse( categoryDTO.created_at() ) );
+        }
+        categoryEntity.descripcion( categoryDTO.descripcion() );
+        categoryEntity.id( categoryDTO.id() );
+        if ( categoryDTO.updated_at() != null ) {
+            categoryEntity.updated_at( LocalDateTime.parse( categoryDTO.updated_at() ) );
+        }
+
+        return categoryEntity.build();
+    }
+}
---
+++ Practica/target/generated-sources/annotations/com/example/Practica/utils/mappers/MarcaMapperImpl.java
@@ -0,0 +1,65 @@
+package com.example.Practica.utils.mappers;
+
+import com.example.Practica.persistence.entity.MarcaEntity;
+import com.example.Practica.presentation.controller.dto.MarcaDTO;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-01-01T18:53:21+0000",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.41.0.v20241217-1506, environment: Java 17.0.13 (Eclipse Adoptium)"
+)
+@Component
+public class MarcaMapperImpl implements MarcaMapper {
+
+    @Override
+    public MarcaDTO fromEntity(MarcaEntity marca) {
+        if ( marca == null ) {
+            return null;
+        }
+
+        Long id = null;
+        String marca1 = null;
+        String descripcion = null;
+        String created_at = null;
+        String updated_at = null;
+
+        id = marca.getId();
+        marca1 = marca.getMarca();
+        descripcion = marca.getDescripcion();
+        if ( marca.getCreated_at() != null ) {
+            created_at = DateTimeFormatter.ISO_LOCAL_DATE_TIME.format( marca.getCreated_at() );
+        }
+        if ( marca.getUpdated_at() != null ) {
+            updated_at = DateTimeFormatter.ISO_LOCAL_DATE_TIME.format( marca.getUpdated_at() );
+        }
+
+        MarcaDTO marcaDTO = new MarcaDTO( id, marca1, descripcion, created_at, updated_at );
+
+        return marcaDTO;
+    }
+
+    @Override
+    public MarcaEntity fromDTO(MarcaDTO marca) {
+        if ( marca == null ) {
+            return null;
+        }
+
+        MarcaEntity.MarcaEntityBuilder marcaEntity = MarcaEntity.builder();
+
+        if ( marca.created_at() != null ) {
+            marcaEntity.created_at( LocalDateTime.parse( marca.created_at() ) );
+        }
+        marcaEntity.descripcion( marca.descripcion() );
+        marcaEntity.id( marca.id() );
+        marcaEntity.marca( marca.marca() );
+        if ( marca.updated_at() != null ) {
+            marcaEntity.updated_at( LocalDateTime.parse( marca.updated_at() ) );
+        }
+
+        return marcaEntity.build();
+    }
+}
