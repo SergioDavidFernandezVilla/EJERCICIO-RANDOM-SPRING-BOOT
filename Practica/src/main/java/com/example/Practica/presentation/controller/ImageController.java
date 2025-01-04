73dc39eeefb3eac0eee17f6df08e1e0f497a1e70
gatocombotasfernandez <gatocombotasfernandez@gmail.com>
2025-01-04 02:39:54 +0000

    "add method getImage"

--- Practica/src/main/java/com/example/Practica/presentation/controller/ImageController.java
+++ Practica/src/main/java/com/example/Practica/presentation/controller/ImageController.java
@@ -1,6 +1,8 @@
 package com.example.Practica.presentation.controller;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -10,6 +12,7 @@ import org.springframework.core.io.UrlResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,34 +27,38 @@ import org.springframework.core.io.Resource;
 @RequestMapping("/upload/image")
 @CrossOrigin(origins = "http://localhost:4200")
 public class ImageController {
-    
+
     private ImageService imageService;
 
     public ImageController(ImageService imageService) {
         this.imageService = imageService;
     }
 
-    @GetMapping
-    public ResponseEntity<Resource> getImage() {
+    // Obtener la imagen
+    @GetMapping("/{fileName:.+}")
+    public ResponseEntity<Resource> getImage(@PathVariable String fileName) {
         try {
-            // Ruta física del archivo (debe coincidir con donde se almacenó la imagen)
-            Path imagePath = Paths.get("uploads").resolve("imagen.jpg").normalize();
-            Resource resource = new UrlResource(imagePath.toUri());
-
-            if (!resource.exists()) {
-                return ResponseEntity.badRequest().body(null);
+            // Obtener la URL del archivo utilizando el nombre
+            String fileUrl = imageService.getFileNameUrl(fileName);
+            
+            if (fileUrl == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
             }
-
-            // Configuración de encabezados para mostrar la imagen
-            return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_TYPE, "image/jpeg") // Cambia "image/jpeg" si es PNG u otro formato
-                    .body(resource);
-
+            
+            Path filePath = Paths.get(fileUrl);
+            UrlResource resource = new UrlResource(filePath.toUri());
+            
+            if (resource.exists() || resource.isReadable()) {
+                return ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                        .body(resource);
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
         } catch (Exception e) {
-            return ResponseEntity.internalServerError().body(null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
-
 
     // CREATE
     @PostMapping
