4e91072ae3cc128231bf26c05c5839e2126c1347
gatocombotasfernandez <gatocombotasfernandez@gmail.com>
2025-01-04 01:16:46 +0000

    "add method in ImageController"

---
+++ Practica/src/main/java/com/example/Practica/presentation/controller/ImageController.java
@@ -0,0 +1,34 @@
+package com.example.Practica.presentation.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.Practica.services.ImageService;
+
+@RestController
+@RequestMapping("/upload/image")
+@CrossOrigin(origins = "http://localhost:4200")
+public class ImageController {
+    
+    @Autowired
+    private ImageService imageService;
+
+    // CREATE
+    @PostMapping
+    public ResponseEntity<String> createImage(@RequestParam("image") MultipartFile file) {
+        try {
+            String fileName = imageService.saveImage(file);
+            return ResponseEntity.ok("Imagen subida exitosamente: " + fileName);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body("Error de validación: " + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error interno del servidor: " + e.getMessage());
+        }
+    }
+}
--- Practica/target/generated-sources/annotations/com/example/Practica/utils/mappers/ProductMapperImpl.java
+++ Practica/target/generated-sources/annotations/com/example/Practica/utils/mappers/ProductMapperImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-01-03T02:48:30+0000",
+    date = "2025-01-04T01:11:00+0000",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.41.0.v20241217-1506, environment: Java 17.0.13 (Eclipse Adoptium)"
 )
 @Component
